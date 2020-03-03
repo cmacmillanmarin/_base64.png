762b65bc852bab823994221026e07401b6d18d76
Christian MacMillan <_@cmacmillanmarin.com>
2020-03-03 09:25:16 +0100

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = ({debug = false} = {}) => {
 					query: {
 						compact: true,
 						presets: [
-							['es2015', {modules: false}]
+							"@babel/preset-env"
 						]
 					}
 				},
